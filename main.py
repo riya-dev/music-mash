404608e507c9b84af8a74449283132c9fe0fd6fc
Riya Dev <riyasdev9@gmail.com>
2023-12-26 12:15:00 -0500

    returns top 10 tracks based on artist search

--- main.py
+++ main.py
@@ -33,12 +33,31 @@ def get_auth_header(token):
 def search_for_artist(token, artist_name):
     url = "https://api.spotify.com/v1/search"
     headers = get_auth_header(token)
-    query = f"q={artist_name}&type=artist&limit=1"
-    query_url = url + "?" + query
+    query = f"?q={artist_name}&type=artist&limit=1"
+    
+    query_url = url + query
     result = get(query_url, headers=headers)
-    json_result = json.loads(result.content)
-    print(json_result)
+    json_result = json.loads(result.content)["artists"]["items"]
+    if len(json_result) == 0:
+        print("No artist with this name exists...")
+        return None
+    
+    return json_result[0]
+
+def get_songs_by_artist(token, artist_id):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+    headers = get_auth_header(token)
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)["tracks"]
+    return json_result
 
 token = get_token()
-print(token)
-search_for_artist(token, "ACDC")
+# print(token)
+result = search_for_artist(token, "Adele")
+# print(result["name"])
+artist_id = result["id"]
+songs = get_songs_by_artist(token, artist_id)
+# print(songs)
+
+for idx, song in enumerate(songs):
+    print(f"{idx + 1}. {song['name']}")
